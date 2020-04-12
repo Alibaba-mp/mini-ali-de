84a34462aa4255d7f7e906af0ed08a75d67bb418
noneven <chenjiancj2011@outlook.com>
2020-04-12 16:50:35 +0800

    ensure action

--- packages/hooks/src/hooks/useReducer.ts
+++ packages/hooks/src/hooks/useReducer.ts
@@ -1,11 +1,12 @@
-import { TAction, TDispatch } from '../interface';
+import { IAction, TAction, TDispatch } from '../interface';
 import { useState } from './useState';
 
-export function useReducer<S, I>(reducer: (state: S, action: TAction, payload: any) => S, initialState: any, initializer?: (state: I) => S): [S, TDispatch] {
+export function useReducer<S, I>(reducer: (state: S, action: TAction, payload?: any) => S, initialState: any, initializer?: (state: I) => S): [S, TDispatch] {
   initialState = typeof initializer === 'function' ? initializer(initialState) : initialState;
   const [state, setState] = useState(initialState);
   const dispatch = (action: TAction, payload?: any) => {
-    const result = reducer(state, action, payload);
+    const ensureAction: IAction = typeof action === 'string' ? { payload, type: action } : action;
+    const result = reducer(state, ensureAction);
     setState(result);
   };
 
