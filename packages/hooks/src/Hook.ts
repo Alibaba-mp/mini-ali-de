1f422617fd9ad8b9ed5994f08c52969b7f3d0f69
noneven <chenjiancj2011@outlook.com>
2020-04-23 01:04:57 +0800

    fix hooks

--- packages/hooks/src/Hook.ts
+++ packages/hooks/src/Hook.ts
@@ -56,8 +56,11 @@ export default class Hook {
       }
       this.isScheduled = false;
       const value = this.run();
+      if (!value) {
+        continue;
+      }
       this.subscribes.forEach(subs => {
-        if (isFunction(subs) && value) {
+        if (isFunction(subs)) {
           subs.bind(this.context)(value);
         }
       });
